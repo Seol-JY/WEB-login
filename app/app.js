800fcc2d5b5199fef21954dfeeff848c2732d380
Seol-JY <wlsdud5654@gmail.com>
2022-03-06 14:31:37 +0900

    forntend javascript

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`));
 app.use("/",home); 
 
 module.exports = app;
