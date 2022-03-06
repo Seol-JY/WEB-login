8185d09b4cdab95406d49ebce932f4d4c08478a8
Seol-JY <wlsdud5654@gmail.com>
2022-03-06 14:57:36 +0900

    DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hell");
+ const email = document.querySelector("#email"), 
+  password = document.querySelector("#password"),
+  loginbtn = document.querySelector("#login");
+
+  loginbtn.addEventListener("click", login);
+
+  function login() {
+      const req = {
+          email: email.value,
+          password: password.value,
+      }
+      console.log(req);
+  };
